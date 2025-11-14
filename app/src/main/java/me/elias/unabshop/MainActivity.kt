69e775fcb8a8aa03e3f52f7c0cb71b19eff3f22c
elias <jcruz895@unab.edu.co>
2025-11-14 04:28:36 -0500

    error solucionado

--- app/src/main/java/me/elias/unabshop/MainActivity.kt
+++ app/src/main/java/me/elias/unabshop/MainActivity.kt
@@ -4,18 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import me.elias.unabshop.ui.theme.UnabShopTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             UnabShopTheme {
-                NavigationApp()
+                AppNavigator()
             }
         }
     }
 }
 
+@Composable
+fun AppNavigator() {
+    val nav = rememberNavController()
 
+    NavHost(
+        navController = nav,
+        startDestination = "login"
+    ) {
+        composable("login") {
+            LoginScreen(
+                onClickRegister = { nav.navigate("register") },
+                onLoginSuccess = { nav.navigate("main") }
+            )
+        }
+
+        composable("register") {
+            RegisterScreen(
+                onClickBack = { nav.popBackStack() },
+                onRegisterSuccess = { nav.navigate("main") }
+            )
+        }
+
+        composable("main") {
+            NavigationApp()
+        }
+    }
+}
